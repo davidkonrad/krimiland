b84550bd902013fbd87944b3f336192df62dfa39
davidkonrad <davidkonrad@gmail.com>
2021-04-06 21:25:28 +0200

    fixed relative stier til billeder (ikke mange tommer ruder længere)

--- fix-billeder.php
+++ fix-billeder.php
@@ -5,7 +5,12 @@ $dom = new DOMDocument('1.0');
 $dom->loadHTMLFile($from);
 $images = $dom->getElementsByTagName('img');
 foreach($images as $image) {
-	$src = str_replace('../', 'estonia%20final%20report/', $image->getAttribute('src')); 
+	$src = $image->getAttribute('src');
+	echo $src.'<br>';
+	$src = str_replace('../', 'estonia%20final%20report/', $src);
+	if (substr($src, 0, 7 ) === 'images/') {
+		$src = 'estonia%20final%20report/'.$src;
+	}
 	$image->setAttribute('src', 'https://www.estoniaferrydisaster.net/'.$src);
 }
 $dom->save($to);
